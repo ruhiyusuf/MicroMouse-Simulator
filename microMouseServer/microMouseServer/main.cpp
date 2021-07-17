f52540df878b1b6c1cf31280b81fd593569965d6
ruhiyusuf <ruhi.yusuf@gmail.com>
2021-07-17 00:09:47 -0700

    fix for maze crash

--- microMouseServer/microMouseServer/main.cpp
+++ microMouseServer/microMouseServer/main.cpp
@@ -1,8 +1,31 @@
 #include "micromouseserver.h"
 #include <QApplication>
 
+#include <stdio.h>
+#include <execinfo.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+void handler(int sig) {
+  void *array[10];
+  size_t size;
+
+  // get void*'s for all entries on the stack
+  size = backtrace(array, 10);
+
+  // print out all the frames to stderr
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  exit(1);
+}
+
+
 int main(int argc, char *argv[])
 {
+    signal(SIGSEGV, handler);   // install our handler
+
     QApplication a(argc, argv);
     microMouseServer w;
     w.show();
