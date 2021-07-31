b2f8516642d99fa06d915874e660fa4e0c29e412
ruhiyusuf <ruhi.yusuf@gmail.com>
2021-07-30 20:01:55 -0700

    changed global to static, updated updateCurrentPos func

--- microMouseServer/microMouseServer/studentai.cpp
+++ microMouseServer/microMouseServer/studentai.cpp
@@ -1,46 +1,92 @@
 
 #include "micromouseserver.h"
 #include <string>
+#include <iostream>
+
+using namespace std;
+// ruhiyusuf
 
 const int NROWS = 20;
 const int NCOLS = 20;
 
+void updateCurrentPos(char cMove, int cDirection, int &currentXPos, int &currentYPos, int visitCount[][NCOLS] ) {
 
-int visitCount[NROWS][NCOLS];
-char move[NROWS][NCOLS];
-bool arrayInitialized = false;
+    switch(cDirection) {
 
-int currentXPos = 0;
-int currentYPos = 0;
-
-char currentMove = 'z';
-int degree = 90;
-
-void initializeArrays() {
-
-    for (int i = 0; i < NROWS; i++) {
-        for (int j = 0; j < NCOLS; j++) {
-            visitCount[i][j] = 0;
-            move[i][j] = 'z'; // 'z' represent invalid move
+    case 0: // North
+        switch(cMove) {
+        case 'f':
+            currentYPos++;
+            break;
+        case 'r':
+            currentXPos++;
+            break;
+        case 'l':
+            currentXPos--;
+            break;
+        case 'b':
+            currentYPos--;
+            break;
         }
+
+        break;
+
+    case 1: // East
+        switch(cMove) {
+        case 'f':
+            currentXPos++;
+            break;
+        case 'r':
+            currentYPos--;
+            break;
+        case 'l':
+            currentYPos++;
+            break;
+        case 'b':
+            currentXPos--;
+            break;
+        }
+        break;
+
+    case 2: // South
+        switch(cMove) {
+        case 'f':
+            currentYPos--;
+            break;
+        case 'r':
+            currentXPos--;
+            break;
+        case 'l':
+            currentXPos++;
+            break;
+        case 'b':
+            currentYPos++;
+            break;
+        }
+        break;
+
+    case 3: // West
+        switch(cMove) {
+        case 'f':
+            currentXPos--;
+            break;
+        case 'r':
+            currentYPos++;
+            break;
+        case 'l':
+            currentYPos--;
+            break;
+        case 'b':
+            currentXPos++;
+            break;
+        }
+        break;
+
     }
 
-    visitCount[0][0] = 1; // initialize robot starting position at [0, 0]
-    arrayInitialized = true;
+
+    visitCount[currentXPos][currentYPos] += 1;
 }
-
-void findFinish() {
-
-}
-
-/*void getCurrentMove(char moveChar) {
-    char moveChar;
-
-    switch(moveChar) {
-    case 'r':
-        turnRight();
-    }
-}*/
 
 void microMouseServer::studentAI()
 {
@@ -64,41 +110,80 @@ void microMouseServer::studentAI()
 */
 
 
-    if (!arrayInitialized) initializeArrays();
+
+
+    static int visitCount[NROWS][NCOLS];
+
+    static int currentXPos = 0;
+    static int currentYPos = 0;
+
+    static bool startFlag = true;
+
+    if (startFlag) {
+        memset(visitCount, 0, sizeof(visitCount[0][0]) * NROWS * NCOLS);
+        visitCount[currentXPos][currentYPos] = 1;
+        startFlag = false;
+    }
+
+    static char currentMove = 'z';
+
+    static bool turnFlag = false;
+
+    static int leftTurns = 0;
+
+    static int prevDirection = 0;
+    static int nextDirection = 0;
+
+    turnFlag = false;
+
+    cout << "Move: " << currentMove << " ";
+    cout << "Direction: " << nextDirection << " ";
+    cout << "x: " << currentXPos << " ";
+    cout << "y: " << currentYPos << " ";
+    cout << "visitCount: " << visitCount[currentXPos][currentYPos];
+    cout << endl;
+
 
     if (!isWallRight()) {
         turnRight();
-        degree = degree - 90;
+        nextDirection = (nextDirection + 1) % 4;
+        turnFlag = true;
+
+        currentMove = 'r';
+        leftTurns = 0;
 
     } else if (isWallForward() && !isWallLeft()) {
         turnLeft();
 
-        degree = degree + 90;
+        turnFlag = true;
+        currentMove = 'l';
+
+        leftTurns += 1;
+        nextDirection = (nextDirection + 3) % 4;
+
     } else if (isWallForward() && isWallLeft() && isWallRight()) {
         turnRight();
         turnRight();
 
-        degree = degree + 180;
+        nextDirection = (nextDirection + 2) % 4;
+
+        turnFlag = true;
+        currentMove = 'b';
+        leftTurns = 0;
     }
 
     moveForward();
-
-    // std::string msg = "Degree: " + std::to_string(degree);
-    // printUI(msg);
-
-
-
-    /*if (!isWallRight()) {
-        currentMove = 'r';
-    } else if (!isWallForward()) {
+    if (!turnFlag) {
         currentMove = 'f';
-    }
-    else if (!isWallLeft()) {
-        currentMove = 'l';
-    } else {
-        currentMove = 'b';
+
+        leftTurns = 0;
     }
 
-    if currentMove = 'r'*/
+    if (leftTurns == 3) {
+        foundFinish();
+    }
 
+    updateCurrentPos(currentMove, prevDirection, currentXPos, currentYPos, visitCount);
+
+    prevDirection = nextDirection;
 }
